a79c1595c5c3b8bc8acfa2e2d5803115e958fa21
project20212 <93455770+project20212@users.noreply.github.com>
2021-10-30 20:30:17 -0400

    Delete Spiderman directory

--- Spiderman/main.js
+++
@@ -1,9 +0,0 @@
-const init = async () => {
-	const config = await (await fetch('config.json')).json();
-	let newHTML = document.documentElement.innerHTML;
-	Object.keys(config).map((key) => {
-		newHTML = newHTML.replace(key, config[key]);
-	});
-	document.documentElement.innerHTML = newHTML;
-};
-init();
